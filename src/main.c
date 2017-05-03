24b1f69cb3cff6bc864cb4d623ec1e5e5dd94944
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-05-02 21:53:35 -0700

    Move on_window_size callback out of __EMSCRIPTEN__, always call but ifdef web-specific parts

--- src/main.c
+++ src/main.c
@@ -2606,35 +2606,6 @@ void maximize_canvas() {
     on_canvassize_changed(0, NULL, NULL);
 }
 
-void on_window_size(GLFWwindow* window, int width, int height) {
-    static int inFullscreen = 0;
-    static int wasFullscreen = 0;
-
-    int isInFullscreen = EM_ASM_INT_V(return !!(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement));
-    if (isInFullscreen && !wasFullscreen) {
-        printf("Successfully transitioned to fullscreen mode!\n");
-        wasFullscreen = isInFullscreen;
-
-        // Set canvas size to full screen, all the pixels
-        EM_ASM("Browser.setCanvasSize(screen.width, screen.height)");
-    }
-
-    if (wasFullscreen && !isInFullscreen) {
-        wasFullscreen = isInFullscreen;
-        maximize_canvas();
-    }
-
-    g->vr.left.viewport[0] = width/2 - g->vr.hResolution/2;
-    g->vr.left.viewport[1] = height/2 - g->vr.vResolution/2;
-    g->vr.left.viewport[2] = g->vr.hResolution/2;
-    g->vr.left.viewport[3] = g->vr.vResolution;
-
-    g->vr.right.viewport[0] = width/2;
-    g->vr.right.viewport[1] = height/2 - g->vr.vResolution/2;
-    g->vr.right.viewport[2] = g->vr.hResolution/2;
-    g->vr.right.viewport[3] = g->vr.vResolution;
-}
-
 EM_BOOL fullscreen_change_callback(int eventType, const EmscriptenFullscreenChangeEvent *event, void *userData) {
     printf("fullscreen_change_callback, isFullscreen=%d\n", event->isFullscreen);
 
@@ -2686,8 +2657,6 @@ void fullscreen_toggle() {
 }
 
 #else // !__EMSCRIPTEN__
-void on_window_size(GLFWwindow* window, int width, int height) {}
-
 void fullscreen_toggle() {
     if (glfwGetWindowMonitor(g->window)) {
         glfwSetWindowMonitor(g->window, NULL, g->window_xpos, g->window_ypos, g->window_width, g->window_height, GLFW_DONT_CARE);
@@ -2698,6 +2667,38 @@ void fullscreen_toggle() {
     }
 }
 #endif
+
+void on_window_size(GLFWwindow* window, int width, int height) {
+#ifdef __EMSCRIPTEN__
+    static int inFullscreen = 0;
+    static int wasFullscreen = 0;
+
+    int isInFullscreen = EM_ASM_INT_V(return !!(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement));
+    if (isInFullscreen && !wasFullscreen) {
+        printf("Successfully transitioned to fullscreen mode!\n");
+        wasFullscreen = isInFullscreen;
+
+        // Set canvas size to full screen, all the pixels
+        EM_ASM("Browser.setCanvasSize(screen.width, screen.height)");
+    }
+
+    if (wasFullscreen && !isInFullscreen) {
+        wasFullscreen = isInFullscreen;
+        maximize_canvas();
+    }
+#endif
+
+    g->vr.left.viewport[0] = width/2 - g->vr.hResolution/2;
+    g->vr.left.viewport[1] = height/2 - g->vr.vResolution/2;
+    g->vr.left.viewport[2] = g->vr.hResolution/2;
+    g->vr.left.viewport[3] = g->vr.vResolution;
+
+    g->vr.right.viewport[0] = width/2;
+    g->vr.right.viewport[1] = height/2 - g->vr.vResolution/2;
+    g->vr.right.viewport[2] = g->vr.hResolution/2;
+    g->vr.right.viewport[3] = g->vr.vResolution;
+}
+
 
 void handle_gamepad_input() {
     if (g->gamepad_connected == -1) return;
