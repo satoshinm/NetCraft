e978f043a0d5aafa93e951a8ff15ad1b4da32967
satoshinm <snmatsutake@yahoo.co.jp>
2017-05-20 20:47:54 -0700

    Add scrollable chat console (up/down keys). Closes GH-83 (#129)
    * Add message_view_index, increase chat buffer but limit visibility
    
    * Add debugging to add_message() to show buffer
    
    * Add arrow keys while chatting to scroll console
    
    * Prevent scrolling past end of ring wraparound
    
    * Prevent scrolling to nonexistent messages
    
    * Remove simulated console for debugging
    
    * Add to readme up/down keys while chatting to scroll history

--- src/config.h
+++ src/config.h
@@ -10,7 +10,8 @@
 #define SCROLL_THRESHOLD 0.1
 #define JUMP_FLY_THRESHOLD 0.2
 #define SCROLL_BLOCK_SELECT true
-#define MAX_MESSAGES 4
+#define MAX_MESSAGES 20
+#define MAX_VISIBLE_MESSAGES 4
 #define DB_PATH "craft.db"
 #ifdef __EMSCRIPTEN__
 #define USE_CACHE false
